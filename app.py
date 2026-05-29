e5f5c4cf083b0d1ddaf5563b5002f7c98c4fd8b4
przemyslawtomanek <przemyslaw.tomanek02@gmail.com>
2026-05-29 09:37:16 +0200

    Updatek: Adding logarithms to calculate the areas of figures

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 from flask import Flask
+from views import views
 
 app = Flask(__name__)
-
-@app.route("/")
-def home():
-  return "First lunch of this site"
+app.register_blueprint(views, ulr_prefix="/")
 
 if __name__ == '__main__':
   app.run(debug=True, port=8000)  
