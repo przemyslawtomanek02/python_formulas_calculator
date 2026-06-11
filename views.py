95b030e98f0d20aabc88b14c20ad9dccbd2d17d0
przemyslawtomanek <przemyslaw.tomanek02@gmail.com>
2026-06-11 09:37:57 +0200

    Connect flask with html form

--- views.py
+++ views.py
@@ -1,7 +1,12 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 views = Blueprint(__name__, "views")
 
-@views.route("/")
+@views.route("/", methods=['GET', 'POST'])
 def home():
-  return render_template("index.html")
+  wynik = None
+  if request.method == 'POST':
+    bok_z_html = request.form.get('side-square')
+    bok = float(bok_z_html)
+    wynik = bok * bok
+  return render_template("index.html", pole=wynik )
