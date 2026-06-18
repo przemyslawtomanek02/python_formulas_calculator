3663fc81ee41139db1c621c1e76d9aadbe31e169
przemyslawtomanek <przemyslaw.tomanek02@gmail.com>
2026-06-18 18:19:54 +0200

    Implement new design.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 from views import views
 
 app = Flask(__name__)
@@ -6,3 +6,7 @@ app.register_blueprint(views, ulr_prefix="/")
 
 if __name__ == '__main__':
   app.run(debug=True, port=8000)  
+
+@app.errorhandler(404)
+def page_not_found(e):
+  return render_template("404.html"), 404
--- views.py
+++ views.py
@@ -5,6 +5,10 @@ import formulas_functions
 
 views = Blueprint(__name__, "views")
 
+@views.route("/")
+def home():
+  return render_template("home.html")
+
 @views.route("/square", methods=['GET', 'POST'])
 def square_area():
   wynik = None
@@ -12,10 +16,10 @@ def square_area():
     bok = request.form.get('side-square')
     if bok == "":
       error = "this is message"
-      return render_template("index.html", square_area=error )
+      return render_template("square.html", square_area=error )
     na_liczbe = float(bok)
     wynik = formulas_functions.square_area(na_liczbe)
-  return render_template("index.html", square_area=wynik )
+  return render_template("square.html", square_area=wynik )
 
 
 @views.route("/rectangle", methods=['GET', 'POST'])
@@ -26,10 +30,10 @@ def rectangle_area():
     h_string = request.form.get('h')
     if w_string == "" and h_string == "":
       error = "this is message"
-      return render_template("index.html", rectangle_area=error )
+      return render_template("rectangle.html", rectangle_area=error )
     w = float(w_string)
     h = float(h_string)
     print("--------------", request.form, "--------------")
     wynik = formulas_functions.rectangle_area(w, h)
-  return render_template("index.html", rectangle_area=wynik)
+  return render_template("rectangle.html", rectangle_area=wynik)
 
